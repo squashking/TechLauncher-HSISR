ac792a7ba8433d003cf94d54632add483e9ae851
FuYunfan <u7635194@anu.edu.au>
2024-08-09 19:03:58 +1000

    create workspace for 2024S2 and upload basic function code

---
+++ Software/Functions/Basic Functions/HSI_MAT_TRANSFER.py
@@ -0,0 +1,97 @@
+import spectral.io.envi as envi
+import numpy as np
+from scipy.io import savemat
+from scipy.io import loadmat
+import h5py
+
+def load_mat_v73(file_path,dataname):
+    with h5py.File(file_path, 'r') as file:
+        data = file[dataname][:]
+        data = data.squeeze()
+        data = data.transpose((2, 1, 0))
+    return data
+
+def hsi_to_mat(bil_path,header_path, filename):
+    hsi = envi.open(header_path, bil_path)
+    bands = range(hsi.nbands)
+    data = hsi.read_bands(bands)
+    data = data.astype(np.float32)
+    wavelengths = hsi.metadata.get('wavelength', [])
+    mat_dict = {'HSI': data, 'wavelength': wavelengths}
+    savemat(filename, mat_dict)
+
+def mat_to_hsi(mat_path,save_path_hsi,tag):
+    try:
+        image_data = loadmat(mat_path)
+        image = image_data[tag]
+        image = image.squeeze()
+    except:
+        image = load_mat_v73(mat_path,tag)
+    image_new = []
+    for i in range(480):
+        image_bond = image[:, :, i]
+        image_bond[image_bond < 0] = 0
+        image_bond = image_bond / np.max(image_bond)
+        image_bond = np.round(image_bond * 4095)
+        image_new.append(image_bond)
+    image = np.stack(image_new,axis=2)
+    envi.save_image(save_path_hsi, image, dtype=np.float32, interleave='bil', ext='bil',force=True)
+    headerPath = 'get_result_data/2024-04-26--17-22-00_round-0_cam-1_tray-1.hdr'
+    dictMeta = read_PSI_header(headerPath)
+    create_envi_header(save_path_hsi, dictMeta)
+
+def read_PSI_header(filePath):
+    data_dict = {}
+    with open(filePath, "r") as file:
+        lines = file.readlines()
+    wavelengths = []
+    for line in lines:
+        parts = line.strip().split(" ")
+        if "WAVELENGTHS" in parts:
+            reading_wavelengths = True
+            continue
+        elif "WAVELENGTHS_END" in parts:
+            reading_wavelengths = False
+            data_dict["WAVELENGTHS"] = wavelengths
+            continue
+        if len(parts) == 2:
+            key, value = parts
+            data_dict[key] = value
+        elif reading_wavelengths:
+            wavelengths.append(float(parts[0]))
+    return data_dict
+
+
+def create_envi_header(filename, dictMeta):
+    with open(filename, 'w') as file:
+        file.write("ENVI\n")
+        file.write("bands = {}\n".format(dictMeta['NBANDS']))
+        file.write("byte order = 0\n")
+        #file.write("data type = {}\n".format(dictMeta['NBITS']))
+        file.write("data type = 4\n")
+        file.write("file type = ENVI Standard\n")
+        file.write("header offset = 0\n")
+        file.write("interleave = {}\n".format(dictMeta['LAYOUT'].lower()))
+        file.write("lines = {}\n".format(str(350)))
+        file.write("samples = {}\n".format(str(500)))
+        file.write("wavelength units = nm\n")
+        file.write("wavelength = {")
+        file.write(','.join(map(str, dictMeta['WAVELENGTHS'])))
+        file.write("}\n")
+
+
+# bil_path ="data/test_path/0ori_data/2021-03-31--12-56-31_round-0_cam-1_tray-Tray_1.bil"
+# header_path = "data/test_path/0ori_data/2021-03-31--12-56-31_round-0_cam-1_tray-Tray_1.hdr"
+# save_path_mat = 'data/test_path/1hst_to_mat/test1.mat'
+# hsi_to_mat(bil_path,header_path,save_path_mat)
+#
+# mat_path = 'data/test_path/1hst_to_mat/test1.mat'
+# save_path_hsi = 'data/test_path/2mat_to_hsi/test1.hdr'
+# mat_to_hsi(mat_path,save_path_hsi)
+
+mat_path = 'data/server_data/00005.mat'
+save_path_hsi = 'Result_flower/Original low resolution HSI file/LR.hdr'
+#save_path_hsi = 'Result/Original low resolution HSI file/result.hdr'
+tag = 'ms'
+mat_to_hsi(mat_path,save_path_hsi,tag)
+
---
+++ Software/Functions/Basic Functions/watch_header.py
@@ -0,0 +1,38 @@
+def read_hdr_file(file_path):
+    metadata = {}
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        if lines[0].strip() != "ENVI":
+            raise ValueError("File does not appear to be an ENVI header (missing 'ENVI' at beginning of first line).")
+        for line in lines[1:]:
+            if '=' in line:
+                key, value = line.strip().split('=', 1)
+                metadata[key.strip()] = value.strip()
+    return metadata
+
+def write_hdr_file(file_path, metadata):
+    with open(file_path, 'w') as file:
+        file.write("ENVI\n")
+        for key, value in metadata.items():
+            file.write(f"{key} = {value}\n")
+
+def modify_hdr_file(file_path, new_metadata):
+    metadata = read_hdr_file(file_path)
+    metadata.update(new_metadata)
+    write_hdr_file(file_path, metadata)
+
+# 使用示例
+hdr_file_path = "Result/SR result/result.hdr"
+new_metadata = {
+    'samples': '1000',  # 修改 samples 的值
+    'lines': '770',     # 修改 lines 的值
+    # 你可以根据需要添加更多的修改
+}
+
+modify_hdr_file(hdr_file_path, new_metadata)
+
+# 读取并显示修改后的 HDR 文件内容
+hdr_metadata = read_hdr_file(hdr_file_path)
+print(hdr_metadata)
+
+# ... existing code ...
